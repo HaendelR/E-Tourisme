7689c66d0aba09948346a223f3389e4364588faf
Haendelr4 <haendelr4@gmail.com>
2023-08-01 21:02:02 +0300

    initialize splash screen
    add splash screen

---
+++ ETourisme/app/src/main/java/first/app/e_tourisme/view/SplashActivity.java
@@ -0,0 +1,29 @@
+package first.app.e_tourisme.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import first.app.e_tourisme.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 2000; // 2 secondes
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
