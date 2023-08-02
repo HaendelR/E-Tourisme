80e1877522e692719d66df985ab253441e327524
mendrikafitahiana <ramilisonfita24@gmail.com>
2023-08-02 12:48:59 +0300

    modifs

--- ETourisme/app/src/main/java/first/app/e_tourisme/tools/CallWebService.java
+++ ETourisme/app/src/main/java/first/app/e_tourisme/tools/CallWebService.java
@@ -10,7 +10,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class CallWebService {
 
-    private String urlServer = "https://9cb9-41-77-16-137.ngrok-free.app/";
+    private String urlServer = "https://9b6a-154-126-66-69.ngrok-free.app/";
     private String authorizationHeader;
 
     public void setAuthorizationHeader(String token) {
