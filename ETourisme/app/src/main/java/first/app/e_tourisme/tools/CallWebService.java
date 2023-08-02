f77b68a55aec156c290bf1183054d6b0ffcf394b
Haendelr4 <haendelr4@gmail.com>
2023-08-02 22:12:04 +0300

    add color

--- ETourisme/app/src/main/java/first/app/e_tourisme/tools/CallWebService.java
+++ ETourisme/app/src/main/java/first/app/e_tourisme/tools/CallWebService.java
@@ -10,7 +10,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class CallWebService {
 
-    private String urlServer = "https://9cb9-41-77-16-137.ngrok-free.app/";
+    private String urlServer = "https://b211-41-77-20-10.ngrok-free.app";
     private String authorizationHeader;
 
     public void setAuthorizationHeader(String token) {
