5d6cc303ea2ab096495ed7ae8eae5f22d6b8cfcf
Haendelr4 <haendelr4@gmail.com>
2023-08-04 09:57:10 +0300

    fix toolbar in site detail

--- ETourisme/app/src/main/java/first/app/e_tourisme/view/DetailActivity.java
+++ ETourisme/app/src/main/java/first/app/e_tourisme/view/DetailActivity.java
@@ -1,10 +1,11 @@
 package first.app.e_tourisme.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import first.app.e_tourisme.R;
 import first.app.e_tourisme.model.TouristicSite;
@@ -16,6 +17,7 @@ public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.details);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         initDetail();
@@ -29,5 +31,18 @@ public class DetailActivity extends AppCompatActivity {
 
     private void initDetail() {
         nomSite = (TextView) findViewById(R.id.nomSite);
+        getSupportActionBar().setTitle("Détail du site");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            // Gérer le clic sur le bouton de retour ici
+            finish(); // Terminez l'activité actuelle pour revenir en arrière
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
