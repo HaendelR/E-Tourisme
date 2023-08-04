1a78bf2527826593209d48c694fc2181504a650b
Haendelr4 <haendelr4@gmail.com>
2023-08-04 11:46:18 +0300

    minor

--- ETourisme/app/src/main/java/first/app/e_tourisme/tools/CallWebService.java
+++ ETourisme/app/src/main/java/first/app/e_tourisme/tools/CallWebService.java
@@ -10,7 +10,7 @@ import cz.msebera.android.httpclient.Header;
 
 public class CallWebService {
 
-    private String urlServer = "https://dc7a-41-77-19-16.ngrok-free.app";
+    private String urlServer = "https://0669-41-77-19-16.ngrok-free.app";
     private String authorizationHeader;
 
     public void setAuthorizationHeader(String token) {
